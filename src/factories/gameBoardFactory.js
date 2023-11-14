a64436b1656fe8f9d87ce2ed1569e932f1b5bacb
Calvin Buckle <ccccbuckle@gmail.com>
2023-11-14 12:52:15 +0200

    setup functioning click events

--- src/factories/gameBoardFactory.js
+++ src/factories/gameBoardFactory.js
@@ -1,35 +1,50 @@
+
+
+
 class Gameboard {
   constructor() {
     this.board = this.generateDomBoard();
     ;
   }
 
+
   generateDomBoard() {
-    
-
-    function generateDivs() {
-      
-      let enemyBoard = document.getElementById('enemyBoard')
-      let playerBoard = document.getElementById('playerBoard')
-
+    let enemyBoard = document.getElementById('enemyBoard')
+let playerBoard = document.getElementById('playerBoard')
       for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
           const divEnemyPlayer = document.createElement('div');
           const divPlayer = document.createElement('div');
-          divEnemyPlayer.textContent = `${i},${j}`;
-          divPlayer.textContent = `${i},${j}`;
+          divEnemyPlayer.id = `enemy(${i},${j})`;
+          divPlayer.id = `player(${i},${j})`;
           playerBoard.appendChild(divPlayer);
-         enemyBoard.appendChild(divEnemyPlayer);
-         
+         enemyBoard.appendChild(divEnemyPlayer);  
+
+           divPlayer.addEventListener('click', this.placeShip)
+           divEnemyPlayer.addEventListener('click', this.attack)
+
         }
       }
-      
     }
-    
-    generateDivs();
-    
+  
+  
+
+
+ attack(event) {
+    // Do something when a div is clicked
+    console.log('Div clicked:', event.target.id);
   }
+ 
+  placeShip(){
+
+  }
+ 
+
+
+
+
 }
+
 
 
 
