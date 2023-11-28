a68b62861be97dd7924c8d58a8d29743334d8ace
Calvin Buckle <ccccbuckle@gmail.com>
2023-11-28 15:16:08 +0200

    fix ai ship placement not clashing

--- src/logic/ai.js
+++ src/logic/ai.js
@@ -1,83 +1,88 @@
 
 import shipList from "../resources/ships";
+//this only runs the code once the gameboard is created
 document.addEventListener('gameboardReady', () => {
 
     let enemyBoard = document.getElementById('enemyBoard');
-    let ship = document.querySelectorAll('.ship');
-    let eBlock = document.querySelectorAll('.eBlock');
-    let shipId;
-    let shipSize;
-   
-
-
-
-
-Carrier
-let carrierIsPlaced = false;
-
-BattleShip
-let battleShipIsPlaced = false;
-
-Cruiser
-let cruiserIsPlaced = false;
-
-Submarine
-let submarineIsPlaced = false;
-
-Destroyer
-let destroyerIsPlaced = false;
-
-
-
-function checkIfPlaced(){
-    if(carrierIsPlaced && battleShipIsPlaced && cruiserIsPlaced
-         && submarineIsPlaced && destroyerIsPlaced !== false)
-    {
-    return true
-    }
-   
-}
-
-
-function randomLoc(){
-    const location = Math.floor(Math.random() * (99 - 0 + 1)) + 0;
-
-    return location
-}
-
     let EnemyCarrier = 'EnemyCarrier';
     let EnemyBattleShip = 'EnemyBattleShip';
     let EnemyCruiser = 'EnemyCruiser';
     let EnemySubmarine = 'EnemySubmarine';
     let EnemyDestroyer = 'EnemyDestroyer';
 
+//this selects a random Location to spawn
+function randomLoc(){
+    const location = Math.floor(Math.random() * (99 - 0 + 1)) + 0;
+    return location
+}
 
+//this places the AI ships
 function aiPlacement(ship){
     let loc = randomLoc();
    let currentEBlock = enemyBoard.firstElementChild;
-   
+   let prevSteps = 0;
+
     //run through the enemy board 
     for(let i = 0; i < loc; i++){
     currentEBlock = currentEBlock.nextElementSibling;
     }
-   
+
+    //this loops and checks if a spot is selected
     for(let j = 0; j < shipList[ship].size; j++){
+        if(currentEBlock.classList.contains('selected')){
+            cleanPrev(currentEBlock,ship,prevSteps)
+          return;
+        }else{
+//this places the ship
+        prevSteps++
         currentEBlock.classList.add('selected')
-        currentEBlock.classList.add(ship)
-       
-        currentEBlock = currentEBlock.nextElementSibling
+        currentEBlock.classList.add(ship)  
+    //this checks if the board isnt ending
+        if(currentEBlock.nextElementSibling){
+         currentEBlock = currentEBlock.nextElementSibling;
+        }else{
+            cleanShips(currentEBlock,ship,prevSteps)
+           return;
+        }
+    }
+    }
+    }
+
+    
+    function cleanPrev(currentEBlock,ship,prevSteps){
+        for(let i = 0; i < prevSteps; i++){
+            currentEBlock = currentEBlock.previousElementSibling;
+            currentEBlock.classList.remove(ship)
+            currentEBlock.classList.remove('selected')
+        }
         
+        
+        return prevSteps = 0, aiPlacement(ship);
+
     }
+
+
+    function cleanShips(currentEBlock,ship,prevSteps){
+        for(let i = 0; i < prevSteps; i++){
+            currentEBlock.classList.remove(ship)
+            currentEBlock.classList.remove('selected')
+            currentEBlock = currentEBlock.previousElementSibling;
+          
+        }
+        return prevSteps = 0, aiPlacement(ship);
     }
-    
-    
- 
-    aiPlacement(EnemyCarrier)
-    aiPlacement(EnemyBattleShip)
-    aiPlacement(EnemyCruiser)
-    aiPlacement(EnemySubmarine)
-    aiPlacement(EnemyDestroyer)
+    function placeShips(){
+
+        setTimeout(aiPlacement(EnemyCarrier),1000)
+        setTimeout(aiPlacement(EnemyBattleShip),1000)
+        setTimeout(aiPlacement(EnemyCruiser),1000)
+        setTimeout(aiPlacement(EnemySubmarine),1000)
+        setTimeout(aiPlacement(EnemyDestroyer),1000)
+
+    }
+    placeShips();
+   
 });
-export default aiPlacement
+export default placeShips
 
 
